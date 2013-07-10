0cf6bf9ffe5690bba528b33077fc129e98e83807
Jeff Tilson <jrtilson@gmail.com>
2013-07-10 17:18:47 -0400

    Only showing most played/featured sidebar items in 'all' context

--- views/default/simplekaltura/featured.php
+++ views/default/simplekaltura/featured.php
@@ -9,24 +9,25 @@
  * @link http://www.thinkglobalschool.com/
  * 
  */
+if (!elgg_get_page_owner_guid()) {
+	$options = array(
+		'type' => 'object',
+		'subtype' => 'simplekaltura_video',
+		'metadata_name' => 'featured_video',
+		'metadata_value' => 'yes',
+		'full_view' => FALSE,
+		'limit' => 5,
+		'offset' => 0,
+		'pagination' => FALSE,
+	);
 
-$options = array(
-	'type' => 'object',
-	'subtype' => 'simplekaltura_video',
-	'metadata_name' => 'featured_video',
-	'metadata_value' => 'yes',
-	'full_view' => FALSE,
-	'limit' => 5,
-	'offset' => 0,
-	'pagination' => FALSE,
-);
+	elgg_push_context('gallery');
+	$videos = elgg_list_entities_from_metadata($options);
+	elgg_pop_context();
 
-elgg_push_context('gallery');
-$videos = elgg_list_entities_from_metadata($options);
-elgg_pop_context();
-
-if ($videos) {
-	echo elgg_view_module('aside', elgg_echo('simplekaltura:label:featured'), $videos, array(
-		'class' => 'simplekaltura-sidebar-gallery-module'
-	));
+	if ($videos) {
+		echo elgg_view_module('aside', elgg_echo('simplekaltura:label:featured'), $videos, array(
+			'class' => 'simplekaltura-sidebar-gallery-module'
+		));
+	}
 }
--- views/default/simplekaltura/most_played.php
+++ views/default/simplekaltura/most_played.php
@@ -9,23 +9,24 @@
  * @link http://www.thinkglobalschool.com/
  * 
  */
+if (!elgg_get_page_owner_guid()) {
+	$options = array(
+		'type' => 'object',
+		'subtype' => 'simplekaltura_video',
+		'order_by_metadata' => array('name' => 'plays', 'as' => 'integer', 'direction' => 'DESC'),
+		'full_view' => FALSE,
+		'limit' => 5,
+		'offset' => 0,
+		'pagination' => FALSE,
+	);
 
-$options = array(
-	'type' => 'object',
-	'subtype' => 'simplekaltura_video',
-	'order_by_metadata' => array('name' => 'plays', 'as' => 'integer', 'direction' => 'DESC'),
-	'full_view' => FALSE,
-	'limit' => 5,
-	'offset' => 0,
-	'pagination' => FALSE,
-);
+	elgg_push_context('gallery');
+	$videos = elgg_list_entities_from_metadata($options);
+	elgg_pop_context();
 
-elgg_push_context('gallery');
-$videos = elgg_list_entities_from_metadata($options);
-elgg_pop_context();
+	$module = elgg_view_module('aside', elgg_echo('simplekaltura:label:mostplayed'), $videos, array(
+			'class' => 'simplekaltura-sidebar-gallery-module'
+	));
 
-$module = elgg_view_module('aside', elgg_echo('simplekaltura:label:mostplayed'), $videos, array(
-		'class' => 'simplekaltura-sidebar-gallery-module'
-));
-
-echo $module;
+	echo $module;
+}
